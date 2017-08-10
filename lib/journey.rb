155c9a39b1545f8ce38da4f428bf193bd37f6ab9
David Divilly <d.divilly@gmail.com>
2017-08-10 16:56:37 +0100

    adds end_joureny

--- lib/journey.rb
+++ lib/journey.rb
@@ -1,10 +1,14 @@
 class Journey
 
+  attr_reader :station_in, :station_out
+
   def initialize(station_in = nil)
     @station_in = station_in
     @station_out = nil
   end
 
-  attr_reader :station_in, :station_out
+  def end_journey(station)
+    @station_out = station
+  end
 
 end
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -2,17 +2,30 @@ require 'journey'
 
 describe Journey do
   let(:journey) { Journey.new "Victoria"}
+  let(:station) {double :station }
 
-  it "takes a starting station" do
-    expect(journey.station_in).to eq "Victoria"
+  context '#initialize' do
+
+    it "takes a starting station" do
+      expect(journey.station_in).to eq "Victoria"
+    end
+
+    it "initializes with nil if no entry station" do
+      expect(Journey.new.station_in).to eq nil
+    end
+
+    it "initializes with an exit station of nil" do
+      expect(journey.station_out).to eq nil
+    end
+
   end
 
-  it "initializes with nil if no entry station" do
-    expect(Journey.new.station_in).to eq nil
-  end
+  context '#end_journey' do
 
-  it "initializes with an exit station of nil" do
-    expect(journey.station_out).to eq nil
+    it "take an end end station" do
+      expect(journey.end_journey(station)).to eq journey.station_out
+    end
+
   end
 
 end
